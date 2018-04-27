ed504b40e36962c3579506ff9c817884e45d5f4a
Christian Loera <chatoloera18@gmail.com>
2018-04-27 08:43:51 -0500

    v1

--- test.py
+++ test.py
@@ -8,26 +8,30 @@
 #       for BROKER_IP
 #
 # Author: HARPi
-# Date: 11.04.2018
+# Date: 25.04.2018
 #=======================================================
 
 # Importing child_mqtt package
 from child_mqtt import child_mqtt
+from time import sleep
 
-NAME = "Test publisher"
-TOPIC = "test/publisher"
-BROKER_IP = "<ENTER_BROKER_IP_ADDRESS>"
-DESC = "A test description"
+NAME = "Matt's Room Temp"
+TOPIC = "HARPi/node_1"
+#BROKER_IP = "<ENTER_BROKER_IP_ADDRESS>"
+BROKER_IP = "192.168.1.176"
+TOKEN = "PIR"
+DESC = "Measure the temperature in Matt's room"
 
 # You don't need to change this
-child = child_mqtt(NAME, TOPIC, BROKER_IP, DESC)
+child = child_mqtt(NAME, TOPIC, BROKER_IP, TOKEN, DESC)
 
-print("My name is: %s" %child.get_name())
-print("My topic is: %s" %child.get_topic())
-print("My IP address: is %s" %child.get_ip())
-print("I am publishing to: %s" %child.get_broker_ip())
-
+child.check_connection()
 child.send_msg("Hello other MQTT")
+sleep(1)
 child.send_msg("Did you receive my message?")
+sleep(1)
 child.send_msg("Disconnecting from: %s" %child.get_broker_ip())
+sleep(1)
 child.disconnect()
+sleep(1)
+child.send_msg("Did this message go to the broker - surprise, no it didn't")
