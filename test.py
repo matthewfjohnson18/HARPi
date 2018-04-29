54ac7d111262e072e275786d8e056a0ace91325f
Christian Loera <chatoloera18@gmail.com>
2018-04-29 13:33:43 -0500

    Update test with child_mqtt updates

--- test.py
+++ test.py
@@ -15,29 +15,22 @@
 from child_mqtt import child_mqtt
 from time import sleep
 
-NAME = "Test publisher"
-TOPIC = "test/publisher"
+NAME = "Test Name"
+TOPIC = "test/topic"
 BROKER_IP = "<ENTER_BROKER_IP_ADDRESS>"
 
 # Tokens can be found in child_mqtt.py
 TOKEN = "<ENTER_TOKEN>"
-DESC = "A test description"
+DESC = "A quick whip descript"
 
 # You don't need to change this
 child = child_mqtt(NAME, TOPIC, BROKER_IP, TOKEN, DESC)
 
-print("My name is: %s" %child.get_name())
-print("My topic is: %s" %child.get_topic())
-print("My IP address: is %s" %child.get_ip())
-print("I am publishing to: %s" %child.get_broker_ip())
-
-child.check_connection()
 child.send_msg("Hello other MQTT")
-sleep(1)
 child.send_msg("Did you receive my message?")
-sleep(1)
-child.send_msg("Disconnecting from: %s" %child.get_broker_ip())
-sleep(1)
-child.disconnect()
-sleep(1)
-child.send_msg("Did this message go to the broker - surprise, no it didn't")
+
+print("Changing status...")
+# Do not include unicode values
+child.status_msg("78 *F")
+child.status_msg("ON")
+child.status_msg("2")
